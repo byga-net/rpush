c6d5a1896531de4c3f9adbdc2171bf8f014e046f
alexmdavis <amdavis@gmail.com>
2024-05-06 13:37:15 -0400

    json_key model validation

--- lib/rpush/client/active_model/fcm/app.rb
+++ lib/rpush/client/active_model/fcm/app.rb
@@ -5,8 +5,7 @@ module Rpush
         module App
           def self.included(base)
             base.instance_eval do
-              # TODO: Add whatever validation is needed here
-              # validates :auth_key, presence: true
+              validates :json_key, presence: true
             end
           end
 
